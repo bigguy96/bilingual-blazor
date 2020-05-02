fcb0864ceb51d94af95873fe47551b8207219abe
GT <gaetan23@msn.com>
2020-05-02 16:36:06 -0400

    managed to get alternate language.

--- LocalizationApp/Utils/JavascriptHelper.cs
+++ LocalizationApp/Utils/JavascriptHelper.cs
@@ -1,7 +1,10 @@
 ﻿using LocalizationApp.Entities;
+using LocalizationApp.Pages;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Html;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
@@ -15,13 +18,41 @@ namespace LocalizationApp.Utils
             var currentCulture = url.Contains("/en/") ? "en" : "fr";
             var alternateCulture = currentCulture.Equals("en") ? "/fr/" : "/en/";
 
+            var segments = url.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            var page = segments[1];
+
+            //if (segments.Length > 1 && segments[1].Length > 1)
+            //{
+
+            //}
+
+            //https://stackoverflow.com/questions/60376576/blazor-how-get-all-route-urls-from-razor-pages-in-blazor-server-side-project
+
+            var routeAttributes1 = Assembly.GetExecutingAssembly().ExportedTypes.Where(t => t.IsSubclassOf(typeof(ComponentBase)));
+
+            foreach(var route in routeAttributes1)
+            {
+                // Now check if this component contains the Authorize attribute
+                var allAttributes = route.GetCustomAttributes(inherit: true);
+                var allAttributes1 = route.GetCustomAttributes(inherit: true).OfType<RouteAttribute>();
+
+                var authorizeDataAttributes = allAttributes.OfType<RouteAttribute>().ToArray();
+
+                foreach(var a in authorizeDataAttributes)
+                {
+                    var b = a;
+                }
+            }
+
             // get @page values from a page and get alternate language.
             var routeAttributes = Assembly.GetExecutingAssembly().ExportedTypes
-                      .Where(t => t.IsSubclassOf(typeof(ComponentBase)))
-                      .Where(x => x.Name.Length > 1)
-                      .Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>().SingleOrDefault(s => s.Template.Contains(alternateCulture)) });
+                     .Where(t => t.IsSubclassOf(typeof(ComponentBase)))
+                     .Where(x => x.Name.Length > 1)
+                     .Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>() });
 
-            var alternateRoute = routeAttributes?.Single(s => s.RouteAttributes != null)?.RouteAttributes.Template;
+            var alternateRoute = routeAttributes
+                .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(url)))?.RouteAttributes
+                .SingleOrDefault(s => s.Template.Contains(alternateCulture))?.Template;
 
             var appTop = new AppTop
             {
