bbd6ef2a5445c2e094f90a30ca27421cfe845147
GT <gaetan23@msn.com>
2020-05-02 16:40:16 -0400

    code cleanup.

--- LocalizationApp/Utils/JavascriptHelper.cs
+++ LocalizationApp/Utils/JavascriptHelper.cs
@@ -1,10 +1,7 @@
 ﻿using LocalizationApp.Entities;
-using LocalizationApp.Pages;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Html;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
 
@@ -12,44 +9,19 @@ namespace LocalizationApp.Utils
 {
     public static class JavascriptHelper
     {
-        public static HtmlString GetAppTop( string url, string applicationName)
+        public static HtmlString GetAppTop(string url, string applicationName)
         {
-
             var currentCulture = url.Contains("/en/") ? "en" : "fr";
             var alternateCulture = currentCulture.Equals("en") ? "/fr/" : "/en/";
 
-            var segments = url.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-            var page = segments[1];
-
-            //if (segments.Length > 1 && segments[1].Length > 1)
-            //{
-
-            //}
-
             //https://stackoverflow.com/questions/60376576/blazor-how-get-all-route-urls-from-razor-pages-in-blazor-server-side-project
-
-            var routeAttributes1 = Assembly.GetExecutingAssembly().ExportedTypes.Where(t => t.IsSubclassOf(typeof(ComponentBase)));
-
-            foreach(var route in routeAttributes1)
-            {
-                // Now check if this component contains the Authorize attribute
-                var allAttributes = route.GetCustomAttributes(inherit: true);
-                var allAttributes1 = route.GetCustomAttributes(inherit: true).OfType<RouteAttribute>();
-
-                var authorizeDataAttributes = allAttributes.OfType<RouteAttribute>().ToArray();
-
-                foreach(var a in authorizeDataAttributes)
-                {
-                    var b = a;
-                }
-            }
-
-            // get @page values from a page and get alternate language.
+            //get @page values from a page and get alternate language.
             var routeAttributes = Assembly.GetExecutingAssembly().ExportedTypes
                      .Where(t => t.IsSubclassOf(typeof(ComponentBase)))
                      .Where(x => x.Name.Length > 1)
                      .Select(s => new { RouteAttributes = s.GetCustomAttributes(inherit: true).OfType<RouteAttribute>() });
-
+            
+            //get route values for the current page and get the alternate language.            
             var alternateRoute = routeAttributes
                 .SingleOrDefault(x => x.RouteAttributes.Any(a => a.Template.Contains(url)))?.RouteAttributes
                 .SingleOrDefault(s => s.Template.Contains(alternateCulture))?.Template;
